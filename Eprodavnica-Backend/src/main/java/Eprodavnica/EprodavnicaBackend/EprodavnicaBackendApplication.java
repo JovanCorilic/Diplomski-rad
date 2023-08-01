7e4024e6a9683fbd2e31c559b184296825666dcb
JovanCorilic <kssbc39@gmail.com>
2023-08-01 10:39:52 +0200

    Inicijalizacija backend dela

---
+++ Eprodavnica-Backend/src/main/java/Eprodavnica/EprodavnicaBackend/EprodavnicaBackendApplication.java
@@ -0,0 +1,13 @@
+package Eprodavnica.EprodavnicaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EprodavnicaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EprodavnicaBackendApplication.class, args);
+	}
+
+}
---
+++ Eprodavnica-Backend/src/test/java/Eprodavnica/EprodavnicaBackend/EprodavnicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Eprodavnica.EprodavnicaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EprodavnicaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
