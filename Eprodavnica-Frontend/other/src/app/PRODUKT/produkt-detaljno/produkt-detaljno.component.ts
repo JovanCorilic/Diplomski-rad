7727a8fede8fa128b42f9355e68c3a112d2291af
JovanCorilic <kssbc39@gmail.com>
2023-09-21 10:31:28 +0200

    hitan fix

--- Eprodavnica-Frontend/other/src/app/PRODUKT/produkt-detaljno/produkt-detaljno.component.ts
+++ Eprodavnica-Frontend/other/src/app/PRODUKT/produkt-detaljno/produkt-detaljno.component.ts
@@ -58,7 +58,7 @@ export class ProduktDetaljnoComponent implements OnInit {
     private router:Router,
     private _snackBar: MatSnackBar
   ){
-    this.pageSize = 3;
+    this.pageSize = 4;
 		this.currentPage = 1;
 		this.totalSize = 1;
 
